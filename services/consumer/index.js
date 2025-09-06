e64b42dd43c96d2115b2f906a903bdbc8434f0de
Vinicius Lage <vinicius_clage@yahoo.com.br>
2025-09-06 17:43:26 -0300

    feat: create consumer logic to update websocket

---
+++ services/consumer/index.js
@@ -0,0 +1,34 @@
+import { SQSClient, ReceiveMessageCommand, DeleteMessageCommand } from '@aws-sdk/client-sqs';
+import { WebSocketServer } from 'ws';
+import 'dotenv/config';
+
+const sqs = new SQSClient({ 
+    endpoint: process.env.LOCALSTACK_ENDPOINT,
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+});
+const wsServer = new WebSocketServer({ port: 8080 });
+
+async function pollQueue() {
+  const data = await sqs.send(new ReceiveMessageCommand({
+    QueueUrl: process.env.SQS_QUEUE_URL,
+    MaxNumberOfMessages: 10,
+    WaitTimeSeconds: 1,
+  }));
+
+  if (data.Messages) {
+    data.Messages.forEach(msg => {
+      wsServer.clients.forEach(client => client.send(msg.Body));
+      sqs.send(new DeleteMessageCommand({ QueueUrl: process.env.SQS_QUEUE_URL, ReceiptHandle: msg.ReceiptHandle }));
+    });
+  }
+}
+
+setInterval(pollQueue, 1000);
+
+wsServer.on('connection', ws => {
+  console.log('Client connected');
+});
